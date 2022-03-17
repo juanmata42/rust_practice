cfaf72c8506b0337c194555cb9451a0b98fd74ff
jtaboada <juanmanuel.taboada@globalincubator.com>
2022-03-17 15:31:31 +0100

    unions done

--- data_structures/src/unions.rs
+++ data_structures/src/unions.rs
@@ -2,6 +2,32 @@
 #![allow(unused_variables)]
 /* use std::mem; */
 
-pub fn unions(){
+// Union is 32bits
+union IntOrFloat {
+    i: i32, //integer
+    f: f32, //float
+}
+
+fn process_value(iof: IntOrFloat) {
+    unsafe {
+        match iof {
+            IntOrFloat { i: 42 } => println!("The meaning of life is 42"),
+            IntOrFloat { f: 3.14 } => println!("The meaning of life is 3.14"),
+            _ => println!("The meaning of life is unknown"),
+        }
+    }
+    /* println!("{}", iof.i);
+    println!("{}", iof.f); */
+}
+
+pub fn unions() {
     println!("Unions----------------------------------");
+    
+    let mut iof = IntOrFloat { i: 123 };
+    iof.i = 42;
+    iof.f = 42.0;
+    
+    let value = unsafe { iof.i };
+    println!("iof.i = {}", value);
+    process_value(iof);
 }
--- data_structures/target/debug/deps/data_structures.d
+++ data_structures/target/debug/deps/data_structures.d
@@ -1,7 +1,8 @@
-C:\Users\juanm\Documents\practica\rust\data_structures\target\debug\deps\data_structures.exe: src\main.rs src\structs.rs src\enums.rs
+C:\Users\juanm\Documents\practica\rust\data_structures\target\debug\deps\data_structures.exe: src\main.rs src\structs.rs src\enums.rs src\unions.rs
 
-C:\Users\juanm\Documents\practica\rust\data_structures\target\debug\deps\data_structures.d: src\main.rs src\structs.rs src\enums.rs
+C:\Users\juanm\Documents\practica\rust\data_structures\target\debug\deps\data_structures.d: src\main.rs src\structs.rs src\enums.rs src\unions.rs
 
 src\main.rs:
 src\structs.rs:
 src\enums.rs:
+src\unions.rs:
