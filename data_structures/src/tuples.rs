bfbd75ee9dcfed3cb8b76ae66f3475673287a86b
jtaboada <juanmanuel.taboada@globalincubator.com>
2022-04-27 08:39:08 +0200

    done tuples

--- data_structures/src/tuples.rs
+++ data_structures/src/tuples.rs
@@ -11,12 +11,17 @@ fn using_tuples() {
     let y = 4;
     let sp = sum_and_product(x, y);
     println!("sp is {:?}", sp);
+    println!("{0} + {1} = {2}, {0} * {1} = {3}", x, y, sp.0, sp.1);
+    println!("sp is {0:?} {1:?}", sp.0, sp.1);
+    //destructuring
+    let (a, b) = sp;
+    println!("a = {}, b = {}", a, b);
+    let sp2 = sum_and_product(4, 7);
+    let combined = (sp, sp2);
+    println!("first combined: {:?}", combined);
 }
 
 pub fn tuples() {
     println!("tuples----------------------------------");
     using_tuples();
-    println!("structures: ");
-    println!("vectors: ");
-    println!("arrays: ");
 }
