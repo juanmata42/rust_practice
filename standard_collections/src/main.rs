288cfb551fca1d704e722855bdb8d486d1632323
Juan Manuel Taboada <juanmanuel.taboada@globalincubator.com>
2022-05-29 19:07:40 +0200

    hashmap halfway

--- standard_collections/src/main.rs
+++ standard_collections/src/main.rs
@@ -17,7 +17,26 @@ fn vectors() {
     for x in &a {
         println!("{}", x);
     }
+    a.push(44);
+    println!("a = {:?}", a);
+    let last_element = a.pop(); // option
+                                // some (44)
+    println!("last element is {:?}, a = {:?}", last_element, a);
+}
+use std::collections::HashMap;
+fn shapes() {
+    let mut shapes = HashMap::new();
+    shapes.insert(String::from("square"), 4);
+    shapes.insert(String::from("triangle"), 3);
+    println!("a square has {} sides", shapes["square"]);
+    //this one checks if 
+    shapes.insert("square".into(),5);
+shapes.entry("circle".into()).or_insert(1);
+    for (key, value) in &shapes {
+        println!("{} has {} sides", key, value);
+    }
 }
 fn main() {
     vectors();
+    shapes();
 }
